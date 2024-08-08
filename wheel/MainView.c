5edfdef4090b212fb519c111bd19395828459e27
JSDale <57218102+JSDale@users.noreply.github.com>
2024-08-08 21:34:39 +0100

    reverted to font 24

--- wheel/MainView.c
+++ wheel/MainView.c
@@ -29,14 +29,9 @@ uint8_t GetNumber(void)
 void DisplayRandomNumbers(void)
 {
 	uint8_t number = GetNumber();
-
-	//POINT xStart = MaxHeight / 2;
-	POINT xStart = 120;
-    //POINT yStart = MaxWidth / 2;
-    POINT yStart = 20;
 	char wibble = (char)number;
     char numbers[] = {wibble, '\0'};
-    GUI_DisString_EN(xStart, yStart, numbers, &Geist, BLACK, WHITE);
+    GUI_DisString_EN(xStart, yStart, numbers, &Font24, BLACK, WHITE);
 }
 
 int Start(void)
